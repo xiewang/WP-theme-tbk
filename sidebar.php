666618cb5f3d676a3f566b0d588d804e7056f524
xiewang <xiewang0501@126.com>
2017-07-13 23:46:49 +0800

    nav style improvement

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 <?php }else { ?>
     <div class="container"  id="cate">
             <ul class="cate-list">
-                <?php wp_nav_menu(array('theme_location'=>'sidebar-menu'));?>
+                <?php wp_nav_menu(array('theme_location'=>'sidebar-menu','walker' => new description_walker() ));?>
             </ul>
             <div class="slider">
             	<div class="banner">
