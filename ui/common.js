c7cdf0deadab524cafd7928cce05d9335f61b0da
xiewang <xiewang0501@126.com>
2017-07-16 00:37:08 +0800

    stopPropagation

--- ui/common.js
+++ ui/common.js
@@ -5,8 +5,15 @@ $(function(){
 		else
 			$('.m-cate-drop').addClass('hide');
 	});
-	$('#m-packup').on('click', function(){
+	$('#m-packup').on('click', function(event){
+		$('.m-cate-drop').addClass('hide');
+		event.stopPropagation(); 
+	});
+	$('.m-cate-drop').on('click', function(event){
 		$('.m-cate-drop').addClass('hide');
 	});
-	
+	$('.m-cate-drop li a').on('click', function(event){
+		event.stopPropagation(); 
+	});
+
 });
