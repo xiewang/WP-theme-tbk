274d62430b2e23f91db16763290a62e75bcd4552
xiewang <xiewang0501@126.com>
2017-07-20 13:06:39 +0800

    change title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta name="renderer" content="webkit">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">	
 
-	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+	<title><?php bloginfo('name'); ?>推荐:<?php wp_title(''); ?> <?php if(wp_title('', false)) { echo ''; } ?> </title>
 	<meta name="keywords" content="<?php echo get_option('keywords'); ?>" />
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="baidu-site-verification" content="XvZ5JrfmTv" />
