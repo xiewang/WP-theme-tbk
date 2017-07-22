aa0c11cd9d89d7d591c6680045c7f31c99fb5735
xiewang <xiewang0501@126.com>
2017-07-22 14:13:02 +0800

    style improvement

--- 404.php
+++ 404.php
@@ -3,9 +3,9 @@
 <div class="layout page padding-large-top padding-large-bottom">
     <div id="content" class="container">
 	<div class="text-center">
-            <h1 class="padding-large-bottom"> 好可惜，您要找宝贝不在！</h1>
+            <h1 class="padding-large-bottom"> 好可惜，您要找的宝贝不在了！</h1>
             <div class="padding">
-                <form class="s" method="get" action="http://xbaba.cn" role="search">
+                <form class="s" method="get" action="<?php bloginfo('url');?>" role="search">
                 	<input class="sy-input" type="search" name="s" placeholder="宝贝搜索...">
                 	<button class="sy-submit" type="submit" role="button"><span class="icon-search"></span></button>
                 </form>
@@ -15,7 +15,7 @@
 
         <div class="line-middle">
             <div class="xl6 xs3 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=3">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=3">
                     <div class="sy-hd pt_3c text-center">
                         <div class="iconfont">&#xe605;</div>
                 	<div class="sy-txt">男装男鞋</div>
@@ -23,7 +23,7 @@
 		</a>
             </div>
             <div class="xl6 xs3 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=4">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=4">
                     <div class="sy-hd pt_beauty text-center">
                         <div class="iconfont">&#xe602;</div>
                 	<div class="sy-txt">女装女鞋</div>
@@ -31,7 +31,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=5">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=5">
                     <div class="sy-hd pt_electronic text-center">
                         <div class="iconfont">&#xe624;</div>
                 	<div class="sy-txt">包包配饰</div>
@@ -39,7 +39,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=6">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=6">
                     <div class="sy-hd pt_phone text-center">
                         <div class="iconfont">&#xe618;</div>
                 	<div class="sy-txt">运动户外</div>
@@ -47,7 +47,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=7">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=7">
                     <div class="sy-hd pt_cloth text-center">
                         <div class="iconfont">&#xe622;</div>
                 	<div class="sy-txt">美妆彩妆</div>
@@ -55,7 +55,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=8">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=8">
                     <div class="sy-hd pt_sport text-center">
                         <div class="iconfont">&#xe679;</div>
                 	<div class="sy-txt">童装母婴</div>
@@ -63,7 +63,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=9">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=9">
                     <div class="sy-hd pt_baby text-center">
                         <div class="iconfont">&#xe619;</div>
                 	<div class="sy-txt">零食茶饮</div>
@@ -71,7 +71,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=10">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=10">
                     <div class="sy-hd pt_family text-center">
                         <div class="iconfont">&#xe651;</div>
                 	<div class="sy-txt">内衣情趣</div>
@@ -79,7 +79,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=11">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=11">
                     <div class="sy-hd pt_game text-center">
                         <div class="iconfont">&#xe609;</div>
                 	<div class="sy-txt">数码家电</div>
@@ -87,7 +87,7 @@
 		</a>
             </div>
             <div class="xl6 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=12">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=12">
                     <div class="sy-hd pt_travel text-center">
                         <div class="iconfont">&#xe62f;</div>
                 	<div class="sy-txt">居家生活</div>
@@ -95,7 +95,7 @@
 		</a>
             </div>
             <div class="xl12 xs2 padding-bottom">
-		<a target="_blank" href="//xbaba.cn/?cat=13">
+		<a target="_blank" href="<?php bloginfo('url');?>/?cat=13">
                     <div class="sy-hd pt_finance text-center">
                         <div class="iconfont">&#xe656;</div>
                 	<div class="sy-txt">汽车用品</div>
