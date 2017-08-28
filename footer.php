55736160ae6d67e563ab9eb2a71003a51f8a4374
xiewang <xiewang0501@126.com>
2017-08-28 22:30:42 +0800

    footer signature

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer padding-large-top padding-large-bottom">
 	<div class="container">
-            <div class="line-middle">
-                <span class="beian">沪ICP备17030262号-1</span>
+            <div class="line-middle text-c">
+                <span class="beian">Copyright © 半刀网 2017, All Rights Reserved. 沪ICP备17030262号-1</span>
             </div>            
 	</div>
     <?php if(!wp_is_mobile()){?>
