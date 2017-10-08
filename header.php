0df4451c0afd1e0d6258e36e67f402d382657e9c
xiewang <xiewang0501@126.com>
2017-10-08 12:03:05 +0800

    auto add to homepage enabled in mobile

--- header.php
+++ header.php
@@ -44,7 +44,9 @@
 			   	android:'为了更方便使用，您可将半刀网收藏到桌面，点击菜单按钮<small> %icon.</small>，然后<strong>添加到主屏幕</strong>（“加快捷方式”）。 <small>部分安卓浏览器可能没有<strong>添加到主屏幕</strong>功能😭，你也可以点击菜单中的“添加到首页导航”、“添加到书签”等类似功能。</small>'
 			   }
 			});
-			addtohome.show();
+			<?php if ( wp_is_mobile() ){ ?>
+				addtohome.show();
+			<?php }?>
 		});
 
 	</script>
